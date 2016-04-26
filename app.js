f744ffc03d42df3e89d710f4c58fc0a200eb2a6d
Mike Carey <MLCarey321@gmail.com>
2016-04-26 13:18:33 -0400

    Removed unnecessary cookie parser; Moved Team/Board creation forms to below drop-downs; Increased drop-down sizes; Auto-create states when creating new board via UI

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
@@ -25,7 +24,6 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -3,50 +3,66 @@ var env = 'development';
 
 app
 .factory('teams', ['$http', function($http) {
-    var o = {
-        teams: [],
-        boards: []
-    };
+  var o = {
+    teams: [],
+    boards: []
+  };
 
-    o.get = function(id) {
-        return $http.get('/teams/' + id).then(function(res) {
-            return res.data;
+  o.getTeam = function(id) {
+    return $http.get('/teams/' + id + '/full').then(function(res) {
+      angular.copy(res.data.boards, o.boards);
+      return res.data;
+    });
+  };
+
+  o.getAll = function() {
+    return $http.get('/teams').then(function(res) {
+      angular.copy(res.data, o.teams);
+      return res.data;
+    });
+  };
+
+  o.addTeam = function(team) {
+    return $http.post('/teams', team).then(function(res) {
+      o.teams.push(res.data);
+      return res.data;
+    });
+  };
+
+  o.addBoard = function(teamId, board) {
+    return $http.post('/teams/' + teamId + '/boards', board).then(function(res) {
+      o.boards.push(res.data);
+      return res.data;
+    }).then(function(board) {
+      return $http.post('/teams/' + teamId + '/boards/' + board._id + '/currentState').then(function(res) {
+        board.currentState = res.data;
+        return board;
+      }).then(function(board) {
+        return $http.post('/teams/' + teamId + '/boards/' + board._id + '/targetState').then(function(res) {
+          board.targetState = res.data;
+          return board;
+        }).then(function(board) {
+          return $http.post('/teams/' + teamId + '/boards/' + board._id + '/awesomeState').then(function(res) {
+            board.awesomeState = res.data;
+            return board;
+          });
         });
-    };
-
-    o.getAll = function() {
-        return $http.get('/teams').then(function(res) {
-            angular.copy(res.data, o.teams);
-            return res.data;
-        });
-    };
-
-    o.create = function(team) {
-        return $http.post('/teams', team).then(function(res) {
-            o.teams.push(res.data);
-            return res.data;
-        });
-    };
-
-    o.addBoard = function(teamId, board) {
-      return $http.post('/teams/' + teamId + '/boards', board).then(function(res) {
-        o.boards.push(res.data);
-        return res.data;
       });
-    };
+    });
+  };
 
-    o.getBoards = function(teamId) {
-      return $http.get('/teams/' + teamId + '/boards').then(function(res) {
-        angular.copy(res.data, o.boards);
-        return res.data;
-      });
-    };
+  o.getBoard = function(teamId, boardId) {
+    return $http.get('/teams/' + teamId + '/boards/' + boardId + '/full').then(function(res) {
+      return res.data;
+    });
+  };
 
-    o.getBoard = function(teamId, boardId) {
-      return $http.get('/teams/' + teamId + '/boards/' + boardId).then(function(res) {
-        return res.data;
-      });
-    };
+  o.getBoards = function(teamId) {
+    return $http.get('/teams/' + teamId + '/boards').then(function(res) {
+      angular.copy(res.data, o.boards);
+      return res.data;
+    });
+  };
 
   return o;
 }]);
@@ -68,7 +84,7 @@ app
             if(!$scope.teamName || $scope.teamName === '') {
                 return;
             }
-            teams.create({
+            teams.addTeam({
                 name: $scope.teamName
             }).then(function(team){
               $scope.teams = teams.teams;
@@ -100,8 +116,7 @@ app
             $scope.data.boardSelect = '';
             return;
           }
-          $scope.team = teams.get($scope.data.teamSelect._id);
-          teams.getBoards($scope.data.teamSelect._id);
+          $scope.team = teams.getTeam($scope.data.teamSelect._id);
           $scope.boards = teams.boards;
           $scope.data.boardSelect = '';
         };
@@ -130,16 +145,6 @@ function($stateProvider, $urlRouterProvider) {
         resolve: {
             postPromise: ['teams', function(teams) {
                 return teams.getAll();
-            }]
-        }
-    })
-    .state('teams', {
-        url: '/teams/{id}',
-        templateUrl: '/teams.html',
-        controller: 'TeamsCtrl',
-        resolve: {
-            team: ['$stateParams', 'teams', function($stateParams, teams) {
-                return teams.get($stateParams.id);
             }]
         }
     });
