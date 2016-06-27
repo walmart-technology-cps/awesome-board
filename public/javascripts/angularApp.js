0cb4233c0b3d48c8207b2de502679371832746ad
Mike Carey <MLCarey321@gmail.com>
2016-06-27 17:13:51 -0400

    Added Achievements to UI

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -9,7 +9,7 @@ app
   };
 
   o.getTeam = function(id) {
-    return $http.get('/teams/' + id).then(function(res) {
+    return $http.get('/teams/' + id + '/full').then(function(res) {
       angular.copy(res.data.boards, o.boards);
       return res.data;
     });
@@ -52,7 +52,7 @@ app
   };
 
   o.getBoard = function(teamId, boardId) {
-    return $http.get('/teams/' + teamId + '/boards/' + boardId).then(function(res) {
+    return $http.get('/teams/' + teamId + '/boards/' + boardId + '/full').then(function(res) {
       return res.data;
     });
   };
@@ -66,12 +66,27 @@ app
 
   o.updateBoard = function(board) {
     return $http.put('/teams/' + board.team + '/boards/' + board._id, board).then(function(res) {
-      return board;
+      return $http.get('/teams/' + board.team + '/boards').then(function(res) {
+        angular.copy(res.data, o.boards);
+        return board;
+      });
     });
   };
 
   o.updateState = function(teamId, boardId, updatedState) {
     return $http.put('teams/' + teamId + '/boards/' + boardId + '/states/' + updatedState._id, updatedState).then(function(res) {
+      return res.data;
+    });
+  };
+
+  o.addAchievement = function(teamId, boardId, achievement) {
+    return $http.post('/teams/' + teamId + '/boards/' + boardId + '/achievements', achievement).then(function(res) {
+      return res.data;
+    });
+  };
+
+  o.getAchievements = function(teamId, boardId) {
+    return $http.get('/teams/' + teamId + '/boards/' + boardId + '/achievements').then(function(res) {
       return res.data;
     });
   };
@@ -121,7 +136,7 @@ app
     };
 
     $scope.getTeam = function() {
-      if(!$scope.data.teamSelect) {
+      if(!$scope.data.teamSelect || ''===$scope.data.teamSelect) {
         $scope.team = null;
         $scope.board = null;
         $scope.boards = [];
@@ -130,6 +145,7 @@ app
       }
       $scope.team = teams.getTeam($scope.data.teamSelect._id);
       $scope.boards = teams.boards;
+      $scope.board = null;
       $scope.data.boardSelect = '';
     };
 
@@ -140,6 +156,9 @@ app
       }
       teams.getBoard($scope.data.teamSelect._id, $scope.data.boardSelect._id).then(function(board) {
         $scope.board = board;
+        if(!$scope.team.name) {
+          $scope.team = $scope.team.$$state.value;
+        }
       });
     };
 
@@ -148,11 +167,42 @@ app
         return;
       }
       teams.updateState($scope.data.teamSelect._id, $scope.data.boardSelect._id, state);
-    }
+    };
+
+    $scope.updateBoard = function(board) {
+      if(!board) {
+        return;
+      }
+      teams.updateBoard(board).then(function(board) {
+        $scope.boards = teams.boards;
+        $scope.board = board;
+        $scope.data.boardSelect = board;
+      });
+    };
+
+    $scope.addAchievement = function() {
+      if(!$scope.team || $scope.data.teamSelect === '' || !$scope.board || $scope.boardSelect === '') {
+        return;
+      }
+      if(!$scope.data.achievementDesc || $scope.data.achievementDesc === '') {
+        return;
+      }
+      var achievement = {
+        title: $scope.data.achievementTitle,
+        description: $scope.data.achievementDesc
+      }
+      teams.addAchievement($scope.data.teamSelect._id, $scope.data.boardSelect._id, achievement).then(function(achievement) {
+        teams.getAchievements($scope.data.teamSelect._id, $scope.data.boardSelect._id).then(function(achievements) {
+          $scope.board.achievements = achievements;
+          $scope.data.achievementTitle = '';
+          $scope.data.achievementDesc = '';
+        })
+      });
+    };
   }
 ]);
 
-app.directive( 'editableTextarea', function() {
+app.directive( 'editableStateDescription', function() {
   return {
     restrict: 'E',
     scope: { value: '=', update: '=' },
@@ -188,8 +238,83 @@ app.directive( 'editableTextarea', function() {
       });
     }
   };
-})
-;
+});
+
+app.directive( 'editableStateTitle', function() {
+  return {
+    restrict: 'E',
+    scope: { value: '=', update: '=' },
+    template: '<span ng-click="edit()" ng-bind="value"></span><input ng-model="value"></input>',
+    link: function ( $scope, element, attrs ) {
+      // Let's get a reference to the input element, as we'll want to reference it.
+      var inputElement = angular.element( element.children()[1] );
+
+      // This directive should have a set class so we can style it.
+      element.addClass( 'edit-in-place' );
+
+      // Initially, we're not editing.
+      $scope.editing = false;
+
+      // ng-click handler to activate edit-in-place
+      $scope.edit = function () {
+        $scope.editing = true;
+
+        // We control display through a class on the directive itself. See the CSS.
+        element.addClass( 'active' );
+
+        // And we must focus the element.
+        // `angular.element()` provides a chainable array, like jQuery so to access a native DOM function,
+        // we have to reference the first element in the array.
+        inputElement[0].focus();
+      };
+
+      // When we leave the input, we're done editing.
+      inputElement.prop( 'onblur', function() {
+        $scope.editing = false;
+        element.removeClass( 'active' );
+        $scope.$parent.updateState($scope.update);
+      });
+    }
+  };
+});
+
+app.directive( 'editableBoardName', function() {
+  return {
+    restrict: 'E',
+    scope: { value: '=', update: '=' },
+    template: '<span ng-click="edit()" ng-bind="value"></span><input ng-model="value"></input>',
+    link: function ( $scope, element, attrs ) {
+      // Let's get a reference to the input element, as we'll want to reference it.
+      var inputElement = angular.element( element.children()[1] );
+
+      // This directive should have a set class so we can style it.
+      element.addClass( 'edit-in-place' );
+
+      // Initially, we're not editing.
+      $scope.editing = false;
+
+      // ng-click handler to activate edit-in-place
+      $scope.edit = function () {
+        $scope.editing = true;
+
+        // We control display through a class on the directive itself. See the CSS.
+        element.addClass( 'active' );
+
+        // And we must focus the element.
+        // `angular.element()` provides a chainable array, like jQuery so to access a native DOM function,
+        // we have to reference the first element in the array.
+        inputElement[0].focus();
+      };
+
+      // When we leave the input, we're done editing.
+      inputElement.prop( 'onblur', function() {
+        $scope.editing = false;
+        element.removeClass( 'active' );
+        $scope.$parent.updateBoard($scope.update);
+      });
+    }
+  };
+});
 
 app.config([
 '$stateProvider',
