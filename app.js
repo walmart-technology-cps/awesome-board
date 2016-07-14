ca10eac86540130dedefa0a3bcab8fc6109138d1
Mike Carey <MLCarey321@gmail.com>
2016-07-14 16:25:54 -0400

    Added environment variable MONGOLAB_URI to app.js for configurability

--- app.js
+++ app.js
@@ -60,6 +60,6 @@ app.use(function(err, req, res, next) {
   });
 });
 
-mongoose.connect('mongodb://localhost/board');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/board');
 
 module.exports = app;
