b32722aa6a07706455b66c21f8d3f88b54b5a9cc
Mike Carey <MLCarey321@gmail.com>
2017-06-28 20:23:16 -0400

    Removed user id from Moods schema, made date & moodString required

--- models/Moods.js
+++ models/Moods.js
@@ -1,12 +1,9 @@
 var mongoose = require('mongoose');
 
 var MoodSchema = new mongoose.Schema({
-  userId: {type: String, required: true},
-  date: Date,
+  date: {type: Date, required: true},
   team: {type: mongoose.Schema.Types.ObjectId, ref: 'Team', required: true},
-  moodText: String
+  moodText: {type: String, required: true}
 });
-
-MoodSchema.index({userId: 1, date: 1}, {unique: true});
 
 mongoose.model('Mood', MoodSchema);
