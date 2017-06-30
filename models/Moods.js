0785db57b639597ace49e14772b071a8199eedef
Mike <MLCarey321@gmail.com>
2017-06-30 08:53:31 -0400

    Adding Slack User ID (not name) into Mood schema (#37)
    Adding Slack User ID (not name) into Mood schema to enable filtering for unique users without easily identifying users.

--- models/Moods.js
+++ models/Moods.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var MoodSchema = new mongoose.Schema({
   date: {type: Date, required: true},
   team: {type: mongoose.Schema.Types.ObjectId, ref: 'Team', required: true},
-  moodText: {type: String, required: true}
+  moodText: {type: String, required: true},
+  userId: String
 });
 
 mongoose.model('Mood', MoodSchema);
